c3f4796eedb9a13f1fe4113a78b0df8f19070d5b
Marco <e11918513@student.tuwien.ac.at>
2021-02-01 23:58:16 +0100

    Added comment to explain

--- prototype.py
+++ prototype.py
@@ -1,3 +1,24 @@
+# From just == and < we are usually able
+# to deduce the behaviour of the other
+# operators
+
+# note that the Base class implements
+# no operator directly, but instead
+# defines them all dependent on each other
+
+# we can thus inherit from the base
+# class and implement any number of
+# comparison operators as long
+# as we imlpement either == or != and
+# < or > we will be able to use them all
+
+# while it might seem like this is standard
+# behaviour in python, try using Derived
+# without inheriting from Base
+# while implementing == and < does also
+# provide implementations for != and >
+# trying <= and >= will result in error
+# because they are not defined
 class Base():
     def __eq__(self, other):
         return not (self != other)
@@ -7,15 +28,16 @@ class Base():
 
     def __gt__(self, other):
         return not (self < other) and not (self == other)
-    
+
     def __ge__(self, other):
         return self > other or self == other
-    
+
     def __lt__(self, other):
         return not (self > other) and not (self == other)
-    
+
     def __le__(self, other):
         return self < other or self == other
+
 
 class Derived(Base):
     def __init__(self, value):
